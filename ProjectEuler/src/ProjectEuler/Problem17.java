ef51c8461b699738262b0918754a2ba1fec8ff41
anpopo <65079652+anpopo@users.noreply.github.com>
2020-10-15 16:25:55 +0900

    ProjectEuler

---
+++ ProjectEuler/src/ProjectEuler/Problem17.java
@@ -0,0 +1,31 @@
+package ProjectEuler;
+
+/*
+		1부터 1000까지 영어로 썼을 때 사용된 글자의 개수는?
+		Problem 17
+		[ProjectEuler.net 원본]
+		출제 일시 : 2012-01-03 19:11:35
+		
+		1부터 5까지의 수를 영어로 쓰면 one, two, three, four, five 이고,
+		각 단어의 길이를 더하면 3 + 3 + 5 + 4 + 4 = 19 이므로 사용된 글자는 모두 19개입니다.
+		
+		1부터 1,000까지 영어로 썼을 때는 모두 몇 개의 글자를 사용해야 할까요?
+		
+		참고: 빈 칸이나 하이픈('-')은 셈에서 제외하며, 단어 사이의 and 는 셈에 넣습니다.
+		  예를 들어 342를 영어로 쓰면 three hundred and forty-two 가 되어서 23 글자,
+		  115 = one hundred and fifteen 의 경우에는 20 글자가 됩니다.
+ */
+
+public class Problem17 {
+
+	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
+		
+		
+		
+		long end = System.currentTimeMillis();
+		
+		System.out.println("ms >> " + (end - start));
+	}
+
+}
